0efef3b510c19a6309921d2e1f80b9f4e9613419
Andrey Kazachenko <clashovcki.a@gmail.com>
2022-07-01 12:53:05 +0300

    Section 9

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { Header, Button, Paragraph, Tag, Rating } from "../components";
+import { withLayout } from "../layout/Layout";
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
 	const [rating, setRating] = useState<number>(4);
 
 	return (
@@ -14,37 +15,9 @@ export default function Home(): JSX.Element {
 				Кнопка 2
 			</Button>
 
-			<Paragraph>
-				Студенты освоят не только hard skills, необходимые для работы
-				веб-дизайнером, но и soft skills — навыки, которые позволят
-				эффективно взаимодействовать в команде с менеджерами,
-				разработчиками и маркетологами. Выпускники факультета могут
-				успешно конкурировать с веб-дизайнерами уровня middle.
-			</Paragraph>
-
-			<Paragraph size="small">
-				Напишу сразу в двух курсах, так как проходил оба. Java будет
-				многим непросвещённым сложновата в изучении, но здесь перевес
-				из-за лидирующего положения языка как самого популярного в
-				программировании. Выбор мой пал на эту профессию еще и потому,
-				что Java-разработчики получают самую большую зарплату. Хотя
-				Python начинает догонять Java по многим моментам, но вот в
-				крупном екоме разработке Джава все-таки остается главенствующей
-				сейчас. Скажу так – полнота программы и интенсивность присуща
-				обоим курсам GeekBrains. Хочу отметить, что с первого дня
-				занятий вы приступаете к практике и получаете опыт коммерческой
-				разработки уже в свое резюме. Скажу вам как прошедший это –
-				реально помогло в трудоустройстве!
-			</Paragraph>
-
-			<Paragraph size="large">
-				Выше указаны программы Adobe InDesign, Adobe Illustrator, Corel
-				Draw и ими можно успешно пользоваться дома или в дороге.
-				Современные ноутбуки хорошо справляются с нагрузкой, так зачем
-				загонять специалиста в душный офис. В этой профессии важным
-				считается вдохновение, поэтому дизайнеры ищут его в разных
-				местах.
-			</Paragraph>
+			<Paragraph size="small">Маленький</Paragraph>
+			<Paragraph>Средний</Paragraph>
+			<Paragraph size="large">Большой</Paragraph>
 
 			<Tag size="small">маленький тэг</Tag>
 			<Tag size="medium">большой тэг</Tag>
@@ -58,3 +31,5 @@ export default function Home(): JSX.Element {
 		</>
 	);
 }
+
+export default withLayout(Home);
