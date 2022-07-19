aef9cc5222fae3e2749b9b2047b215c3239fe842
Andrey Kazachenko <clashovcki.a@gmail.com>
2022-07-19 15:30:30 +0300

    Section 16

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -2,71 +2,85 @@ import { RatingProps } from "./Rating.props";
 import styles from "./Rating.module.css";
 import cn from "classnames";
 import StarIcon from "./star.svg";
-import { useEffect, useState, KeyboardEvent } from "react";
+import {
+	useEffect,
+	useState,
+	KeyboardEvent,
+	forwardRef,
+	ForwardedRef,
+} from "react";
 
-export const Rating = ({
-	isEditable = false,
-	rating,
-	setRating,
-	...props
-}: RatingProps): JSX.Element => {
-	const [ratingArray, setRatingArray] = useState<JSX.Element[]>(
-		new Array(5).fill(<></>)
-	);
+export const Rating = forwardRef(
+	(
+		{ isEditable = false, rating, setRating, error, ...props }: RatingProps,
+		ref: ForwardedRef<HTMLDivElement>
+	): JSX.Element => {
+		const [ratingArray, setRatingArray] = useState<JSX.Element[]>(
+			new Array(5).fill(<></>)
+		);
 
-	useEffect(() => {
-		constructRating(rating);
-	}, [rating]);
+		useEffect(() => {
+			constructRating(rating);
+		}, [rating]);
 
-	const constructRating = (currentRating: number) => {
-		const updateArray = ratingArray.map((r: JSX.Element, i: number) => {
-			return (
-				<span
-					className={cn(styles.star, {
-						[styles.filled]: i < currentRating,
-						[styles.editable]: isEditable,
-					})}
-					onMouseEnter={() => changeDisplay(i + 1)}
-					onMouseLeave={() => changeDisplay(rating)}
-					onClick={() => onclick(i + 1)}>
-					<StarIcon
-						tabIndex={isEditable ? 0 : -1}
-						onKeyDown={(e: KeyboardEvent<SVGElement>) =>
-							isEditable && handleSpace(i + 1, e)
-						}
-					/>
-				</span>
-			);
-		});
-		setRatingArray(updateArray);
-	};
+		const constructRating = (currentRating: number) => {
+			const updateArray = ratingArray.map((r: JSX.Element, i: number) => {
+				return (
+					<span
+						className={cn(styles.star, {
+							[styles.filled]: i < currentRating,
+							[styles.editable]: isEditable,
+						})}
+						onMouseEnter={() => changeDisplay(i + 1)}
+						onMouseLeave={() => changeDisplay(rating)}
+						onClick={() => onclick(i + 1)}>
+						<StarIcon
+							tabIndex={isEditable ? 0 : -1}
+							onKeyDown={(e: KeyboardEvent<SVGElement>) =>
+								isEditable && handleSpace(i + 1, e)
+							}
+						/>
+					</span>
+				);
+			});
+			setRatingArray(updateArray);
+		};
 
-	const changeDisplay = (i: number) => {
-		if (!isEditable) {
-			return;
-		}
-		constructRating(i);
-	};
+		const changeDisplay = (i: number) => {
+			if (!isEditable) {
+				return;
+			}
+			constructRating(i);
+		};
 
-	const onclick = (i: number) => {
-		if (!isEditable || !setRating) {
-			return;
-		}
-		setRating(i);
-	};
+		const onclick = (i: number) => {
+			if (!isEditable || !setRating) {
+				return;
+			}
+			setRating(i);
+		};
 
-	const handleSpace = (i: number, e: KeyboardEvent<SVGElement>) => {
-		if (e.code != "Space" || !setRating) {
-			return;
-		}
-		setRating(i);
-	};
+		const handleSpace = (i: number, e: KeyboardEvent<SVGElement>) => {
+			if (e.code != "Space" || !setRating) {
+				return;
+			}
+			setRating(i);
+		};
 
-	return (
-		<div {...props}>
-			{ratingArray.map((r, i) => (
-				<span key={i}>{r}</span>
-			))}
-		</div>
-	);
-};
+		return (
+			<div
+				{...props}
+				ref={ref}
+				className={cn(styles.ratingWrapper, {
+					[styles.error]: error,
+				})}>
+				{ratingArray.map((r, i) => (
+					<span key={i}>{r}</span>
+				))}
+				{error && (
+					<span className={styles.errorMessage}>{error.message}</span>
+				)}
+			</div>
+		);
+	}
+);
--- components/TextArea/TextArea.tsx
+++ components/TextArea/TextArea.tsx
@@ -1,10 +1,26 @@
 import { TextAreaProps } from "./TextArea.props";
 import styles from "./TextArea.module.css";
 import cn from "classnames";
+import { ForwardedRef, forwardRef } from "react";
 
-export const TextArea = ({
-	className,
-	...props
-}: TextAreaProps): JSX.Element => {
-	return <textarea className={cn(className, styles.textarea)} {...props} />;
-};
+export const TextArea = forwardRef(
+	(
+		{ className, error, ...props }: TextAreaProps,
+		ref: ForwardedRef<HTMLTextAreaElement>
+	): JSX.Element => {
+		return (
+			<div className={cn(className, styles.textareaWrapper)}>
+				<textarea
+					ref={ref}
+					className={cn(styles.textarea, {
+						[styles.error]: error,
+					})}
+					{...props}
+				/>
+				{error && (
+					<span className={styles.errorMessage}>{error.message}</span>
+				)}
+			</div>
+		);
+	}
+);
